0aa47865f11d825997b5642841c4454d015a03b6
Eve-LynBremner <emvanschoor@gmail.com>
2026-04-02 09:51:18 +0100

    add category to seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,18 +2,22 @@
 const sequelize = require("../config/connection");
 
 // import models
-const { Post } = require("../models");
+const { Post, Category } = require("../models");
 
 // add data and seeding for Category model
 
 // import seed data
 const postData = require("./posts.json");
 
+const categoryData = require("./categories.json");
+
 // Seed database
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await Post.bulkCreate(postData);
+
+  await Category.bulkCreate(categoryData);
 
   process.exit(0);
 };
